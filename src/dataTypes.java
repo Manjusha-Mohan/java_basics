8470b8cde651a0faabf6526e1945e0c546985ee5
Manjusha <manjushakmohan7@gmail.com>
2024-09-14 18:14:11 +0400

    data types

--- src/dataTypes.java
+++ src/dataTypes.java
@@ -1,5 +1,35 @@
 public class dataTypes {
     public static void main(String[] args) {
+        //Integer type
+        //-128 to 127
+        byte byteNum = 100;
+        // -32768 to 32767
+        short shortNum = 5000;
+        // -2147483648 to 2147483647
+        int intNum = 10000;
+        //-9223372036854775808 to 9223372036854775807
+        long lotNum = 15000000000L;
+
+
+        //floating point type
+        float floatNum = 5.2f;
+        double doubleNum = 5.234d;
+
+        //boolean type
+        boolean isTrue = false;
+
+        //char type
+        char myGrade = 'B';
+        //also used to store ASCII values
+        char value = 65; // A
+        System.out.println(value);
+
+
+        //String
+        String greeting = "Good Morning";
+        
+
+
 
     }
 }
